d652bcd4f58441f355d06ddf32f63247d659e781
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-12 04:11:35 +0000

    Add sample code for non-blocking IO and noecho

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ## Links
 Non-blocking IO
-http://www.flipcode.com/archives/_kbhit_for_Linux.shtml
+http://www.flipcode.com/archives/_kbhit_for_Linux.shtml   
+
+Disable echo to stdout
+https://www.gnu.org/savannah-checkouts/gnu/libc/manual/html_node/getpass.html
 
 
--- tetris.cpp
+++ tetris.cpp
@@ -1,8 +1,14 @@
 #include <iostream> 
 #include <cstdio>
 #include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <stropts.h>
+#include <sys/ioctl.h>
 
 using namespace std;
+
+int _kbhit(void);
 
 // Standard size
 #define GRID_WIDTH 10
@@ -11,11 +17,9 @@ using namespace std;
 int grid1[GRID_WIDTH][GRID_HEIGHT] = { 0 };
 int grid2[GRID_WIDTH][GRID_HEIGHT] = { 0 };
 
-void print_grid(void)
-{
+void print_grid(void) {
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-
 	int i, j;
 	// Iterate backwards i.e. top to bottom
 	for(j=GRID_HEIGHT; j--;)
@@ -57,6 +61,35 @@ int main(int argc, char **argv) {
 		print_grid();
 		sleep(1);
 	}
+
+	struct termios term_old, term_new;
+	while(1)
+	{
+
+		// Turn off echoing else fail
+		if (tcgetattr (fileno (stdout), &term_old) != 0)
+			return -1;
+
+		term_new = term_old;
+		term_new.c_lflag &= ~ECHO;
+		if (tcsetattr (fileno (stdout), TCSAFLUSH, &term_new) != 0)
+			return -1;
+
+
+		if( ! _kbhit() )
+		{
+			char val;
+			scanf(" %c", &val);
+			printf("Value pressed: %c\n", val);
+			break;
+		}
+
+	}
+	// Restore echo
+	(void) tcsetattr (fileno (stdout), TCSAFLUSH, &term_old);
+	fflush(stdout);
+
+	usleep(2000);
 	return 0;
 }
 
@@ -72,3 +105,27 @@ Want to compare the future position of the bottom-most row *in each column* of t
 	(Left->right, top->bottom)
 	Can we do this left to right?
 */
+/**
+ Linux (POSIX) implementation of _kbhit().
+ Morgan McGuire, morgan@cs.brown.edu
+ */
+
+
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (! initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
