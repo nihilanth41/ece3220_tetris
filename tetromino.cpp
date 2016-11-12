7759a0a2753035dce574e4e0be7606888ca45be7
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-12 07:17:34 +0000

    Alternative class design

--- tetromino.cpp
+++ tetromino.cpp
@@ -7,22 +7,34 @@ class Tetromino {
 		virtual void rotate()=0;
 };
 
-// "I"
-class Tetro_I : public Tetromino {
-	private:
+// Tetrominos with bounding square 4^2
+class Tetro_4 : public Tetromino {
+	// Accessible by derived classes
+	protected:
 		static const int bound_height = 4;
 		static const int bound_width = 4;
-		// Current rotation 
-		int current_pos;
 		int square[bound_height][bound_width];
 	public:
-		Tetro_I();
-		virtual void rotate(void);
-		void print();
-		~Tetro_I();
+		void print(void);
+		Tetro_4();
+		~Tetro_4();
+
 };
 
-Tetro_I::Tetro_I() {
+void Tetro_4::print(void) {
+	for(int j=bound_height; j--;)
+	{
+		printf("[");
+		for(int i=0; i<bound_width; i++)
+		{
+			printf(" %d ", square[j][i]);
+		}
+		printf("]\n");
+	}
+	printf("\n");
+}
+
+Tetro_4::Tetro_4() {
 	//Initialize to 0;
 	for(int j=bound_height; j--;)
 	{
@@ -31,7 +43,22 @@ Tetro_I::Tetro_I() {
 			square[j][i] = 0;
 		}
 	}
+}
 
+Tetro_4::~Tetro_4() {
+}
+
+// "I"
+class Tetro_I : public Tetro_4 {
+	private:
+		int current_pos;
+	public:
+		Tetro_I();
+		void rotate(void);
+		~Tetro_I();
+};
+
+Tetro_I::Tetro_I() {
 	// Vertical position 
 	current_pos = 1;
 	square[bound_height-1][bound_width-2] = 1;
@@ -39,10 +66,28 @@ Tetro_I::Tetro_I() {
 	square[bound_height-3][bound_width-2] = 1;
 	square[bound_height-4][bound_width-2] = 1;
 }
-
 Tetro_I::~Tetro_I() {
 }
-
+//	//Initialize to 0;
+//	for(int j=bound_height; j--;)
+//	{
+//		for(int i=0; i<bound_width; i++)
+//		{
+//			square[j][i] = 0;
+//		}
+//	}
+//
+//	// Vertical position 
+//	current_pos = 1;
+//	square[bound_height-1][bound_width-2] = 1;
+//	square[bound_height-2][bound_width-2] = 1;
+//	square[bound_height-3][bound_width-2] = 1;
+//	square[bound_height-4][bound_width-2] = 1;
+//}
+//
+//Tetro_I::~Tetro_I() {
+//}
+//
 void Tetro_I::rotate(void) {
 	if(current_pos == 1)
 	{
@@ -70,22 +115,26 @@ void Tetro_I::rotate(void) {
 	}
 		
 }
-
-void Tetro_I::print(void) {
-	for(int j=bound_height; j--;)
-	{
-		printf("[");
-		for(int i=0; i<bound_width; i++)
-		{
-			printf(" %d ", square[j][i]);
-		}
-		printf("]\n");
-	}
-	printf("\n");
-}
+//
+//void Tetro_I::print(void) {
+//	for(int j=bound_height; j--;)
+//	{
+//		printf("[");
+//		for(int i=0; i<bound_width; i++)
+//		{
+//			printf(" %d ", square[j][i]);
+//		}
+//		printf("]\n");
+//	}
+//	printf("\n");
+//}
 
 int main(void) {
 
+//	Tetro_I t = Tetro_I();
+//	t.print();
+//	t.rotate();
+//	t.print();
 	Tetro_I t = Tetro_I();
 	t.print();
 	t.rotate();
