6a87fd3e7fe2f7b36b46196194e1320b78a05dc2
Zachary Rump <zrrm74@mail.missouri.edu>
2016-12-03 13:21:02 -0600

    Modify Shapes.h

--- include/Shapes.h
+++ include/Shapes.h
@@ -1,10 +1,13 @@
 #ifndef __SHAPES_H
 #define __SHAPES_H
+#include <vector>
+
+using namespace std;
 
 class Shapes {
 	public: 
 		Shapes();
 		vector<vector<vector<vector<int>>>> mShapes;
-}
+};
 
 #endif
